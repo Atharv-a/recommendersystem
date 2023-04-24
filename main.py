15bc123b76e245aa76623fe082d17858817a86b5
Atharva Singh <atharva9arvin@gmail.com>
2023-04-24 19:06:14 +0530

    builts a web application

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+
 uri = "mongodb+srv://Atharva:0987A@cluster.cpafhoe.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
